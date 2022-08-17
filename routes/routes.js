13086d820dc5309efab6bc9ee19170c48eb21a92
PATRICK MACEDO <pjmviana@cda-pa.g12.br>
2022-08-17 10:57:12 -0300

    Refatoracao completa no arquivo de rotas routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 /* Essa é uma classe responsavel pelo gerenciamento das operacoes no banco de dados com base nas rotas acessadas */
-//?--------------------QUERIES----------------------------------
-//CarregaNomesSalas
+
+
 router.get("/nomesSalas", async function (req, res, next) {
   try {
     res.status(200);
@@ -121,93 +121,6 @@ router.post("/balancoDia", async function (req, res, next) {
   } catch (err) {
     res.status(404);
     console.error(`Erro ao carregar os dados.`, err.message);
-    next(err);
-  }
-});
-
-//?-------------------------------------------------------------
-
-/* Efetua a busca das informações*/
-router.post("/search", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.search(req.query.page, req.body));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao carregar os dados.`, err.message);
-    next(err);
-  }
-});
-
-/* POST: Faz login no sistema */
-router.post("/login", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.login(req.body));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao efetuar o cadastro.`, err.message);
-    next(err);
-  }
-});
-
-/* POST: Cria um novo cadastro de usuário*/
-router.post("/create/user", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.create(req.body));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao efetuar o cadastro do usuário.`, err.message);
-    next(err);
-  }
-});
-
-/* POST: Cria um novo cadastro de viagem*/
-router.post("/create/travel", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.createTravel(req.body));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao efetuar o cadastro da viagem.`, err.message);
-    next(err);
-  }
-});
-
-/* PUT: Atualiza as informacoes do cadastro */
-router.put("/update/:id", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.update(req.params.id, req.body));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao atualizar dados`, err.message);
-    next(err);
-  }
-});
-
-/* DELETE: Remove as informacoes cadastradas */
-router.delete("/delete/:id", async function (req, res, next) {
-  try {
-    res.status(200);
-    res.json(await control.remove(req.params.id));
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao remover o registro`, err.message);
-    next(err);
-  }
-});
-
-/* GET: Captura o cep da API da via Cep */
-router.get("/cep/:cep", async function (req, res, next) {
-  try {
-    res.status(200);
-    let data = await control.getCep(req.params.cep);
-    res.send(data);
-  } catch (err) {
-    res.status(404);
-    console.error(`Erro ao buscar endereço. Verifique os dados e tente novamente.`, err.message);
     next(err);
   }
 });
