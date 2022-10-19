ceadb36e9086fe0c7b34e7f790f041eb716849eb
PATRICK MACEDO <pjmviana@cda-pa.g12.br>
2022-10-19 10:36:07 -0300

    Adicionado arquivos e pastas para serem ignorados no git

--- config.js
+++ config.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 //Classe que define a configuracao de acesso ao banco de dados do servidor
 const config = {
-    db: {
-      host: process.env.DB_HOST, //"localhost",
-      user: process.env.DB_USER, //"root",
-      password: process.env.DB_PASS, //"",
-      database: process.env.DB_NAME, //"iboardjs",
-    },
-  
-    local: {
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "iboardjs",
-    },
-    listPerPage: 10,
-  };
-  module.exports = config;
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+
+  local: {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "iboardjs",
+  },
+
+  dev: {
+    host: process.env.DB_HOST_DEV,
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASS_DEV,
+    database: process.env.DB_NAME_DEV,
+  },
+  listPerPage: 10,
+};
+module.exports = config;
